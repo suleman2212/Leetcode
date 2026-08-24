01c0a8b48ecc4b86e66a0bf6f911c22f98548566
suleman2212 <ss2212693@gmail.com>
2026-08-25 00:00:44 +0530

    Time: 0 ms (100%), Space: 43.3 MB (41.35%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean stoneGame(int[] piles) 
+    {
+        // int left=0;
+        // int right=piles.length-1;
+        // int alice=0;
+        // int bob=0;
+        // boolean turn=true;
+        // while(left<right)
+        // {
+        //     if(turn)
+        //     {
+        //         if(piles[left]>piles[right])
+        //         {
+        //             alice+=piles[left++];
+        //         }else{
+        //             alice+=piles[right--];
+        //         }
+        //     }else{
+        //         if(piles[left]>piles[right])
+        //         {
+        //             bob+=piles[left++];
+        //         }else{
+        //             bob+=piles[right--];
+        //         }
+        //     }
+        //     turn=!turn;
+            
+        // }
+        // if(alice>bob)
+        // {
+        //     return true;
+        // }
+        return true;
+    }
+}
